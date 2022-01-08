b4d7c50b8f2f3e36e802c0ca073138e24b0318a0
jieyul <legiyl94@gmail.com>
2022-01-08 09:32:43 +0900

    deploy | 0.0.37

--- build.gradle.kts
+++ build.gradle.kts
@@ -37,7 +37,7 @@ sourceSets {
 }
 
 group = "team.sopo"
-version = "0.0.36"
+version = "0.0.37"
 java.sourceCompatibility = JavaVersion.VERSION_11
 
 repositories {
--- buildscript/dockerBuild_prelive.sh
+++ buildscript/dockerBuild_prelive.sh
@@ -2,7 +2,7 @@
 cd ..
 #./gradlew clean
 ./gradlew bootJar -Pprofile=prelive
-docker rmi www.sooopo.com/sopo_prelive/parcel:0.0.36
-docker build -f ./src/main/resources-prelive/Dockerfile . -t www.sooopo.com/sopo_prelive/parcel:0.0.36
-docker push www.sooopo.com/sopo_prelive/parcel:0.0.36
+docker rmi www.sooopo.com/sopo_prelive/parcel:0.0.37
+docker build -f ./src/main/resources-prelive/Dockerfile . -t www.sooopo.com/sopo_prelive/parcel:0.0.37
+docker push www.sooopo.com/sopo_prelive/parcel:0.0.37
 exit 0
