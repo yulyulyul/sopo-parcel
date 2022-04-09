fdaf9b22e9415ca1aef77aa3eddc97dabdd138be
jieyul <legiyl94@gmail.com>
2022-04-09 14:55:58 +0900

    deploy | 0.0.48

--- buildscript/dockerBuild_prelive.sh
+++ buildscript/dockerBuild_prelive.sh
@@ -2,7 +2,7 @@
 cd ..
 #./gradlew clean
 ./gradlew bootJar -Pprofile=prelive
-docker rmi www.sooopo.com/sopo_prelive/parcel:0.0.47
-docker build -f ./src/main/resources-prelive/Dockerfile . -t www.sooopo.com/sopo_prelive/parcel:0.0.47
-docker push www.sooopo.com/sopo_prelive/parcel:0.0.47
+docker rmi www.sooopo.com/sopo_prelive/parcel:0.0.48
+docker build -f ./src/main/resources-prelive/Dockerfile . -t www.sooopo.com/sopo_prelive/parcel:0.0.48
+docker push www.sooopo.com/sopo_prelive/parcel:0.0.48
 exit 0
--- buildscript/dockerBuild_stage.sh
+++ buildscript/dockerBuild_stage.sh
@@ -2,8 +2,8 @@
 cd ..
 #./gradlew clean
 ./gradlew bootJar -Pprofile=stage
-docker rmi www.sooopo.com/sopo_stage/parcel:0.0.47
-docker build -f ./src/main/resources-stage/Dockerfile . -t www.sooopo.com/sopo_stage/parcel:0.0.47
+docker rmi www.sooopo.com/sopo_stage/parcel:0.0.48
+docker build -f ./src/main/resources-stage/Dockerfile . -t www.sooopo.com/sopo_stage/parcel:0.0.48
 
 #stack_sopo=$(docker stack ls --format "{{.Name}}")
 #
